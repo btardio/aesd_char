13c23843b2411b57f63307c9dcff25d92fb9fd26
Brandon Tardio <btardio@slackware.lan>
2025-10-10 21:02:27 -0700

    updates

--- src/aesd-driver.h
+++ src/aesd-driver.h
@@ -1,9 +1,9 @@
 #ifndef AESD_DRIVER_H
 
 #ifdef __KERNEL__
-int create_pid_buffer(struct aesd_dev* dev, struct aesd_circular_buffer* buffer, char __user *buf, int pid_index);
+int create_pid_buffer(struct aesd_dev* dev, struct aesd_circular_buffer* buffer, char __user *userbuf, int pid_index);
 #else
-int create_pid_buffer(struct aesd_dev* dev, struct aesd_circular_buffer* buffer, char *buf, int pid_index);
+int create_pid_buffer(struct aesd_dev* dev, struct aesd_circular_buffer* buffer, char *userbuf, int pid_index);
 extern int test_variable_offset;
 extern int test_variable_total_size;
 #endif
